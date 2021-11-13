0da561f40bebb0d011813533062af958800c3c03
Caglar <ahmetcmirza@gmail.com>
2021-11-14 00:37:18 +0100

    V2

--- Frontend_eindproject/app.js
+++ Frontend_eindproject/app.js
@@ -4,11 +4,6 @@ function  myFunction(){
 }
 
 
-
-// let fullname = document.getElementById('name').value;
-// let email = document.getElementById('email').value;
-// let phone = document.getElementById('phone').value;
-// info
 let infoname = document.getElementById('infoname');
 let infoemail = document.getElementById('infoemail');
 let infophone = document.getElementById('infophone');
@@ -16,15 +11,18 @@ let infophone = document.getElementById('infophone');
 
 
 function myConfirm(){
-    
+
     infoname= document.getElementById('name').value;
     infoemail = document.getElementById('email').value;
     infophone = document.getElementById('phone').value;
     
 
-    alert(`Name :${infoname}\n
-Email :${infoemail}\n
-Phone :${infophone}`)
-        
-    
+    alert(`Name  : ${infoname}\n
+Email  : ${infoemail}\n
+Phone  : ${infophone}`)
+
+document.getElementById('name').value = "";
+document.getElementById('email').value = "";
+document.getElementById('phone').value = "";
+
 }
